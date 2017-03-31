b2d93c384bd988391a93672b3e166bd8b75fb9d7
Stephen Luke Martin <stephenlmartin@student.cofo.edu>
2017-03-30 21:53:50 -0500

    Weekly update

---
+++ NerdHerd/Controllers/TechniciansController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NerdHerd.Models;
+
+namespace NerdHerd.Controllers
+{
+    public class TechniciansController : Controller
+    {
+        private NerdHerdEntities db = new NerdHerdEntities();
+
+        // GET: Technicians
+        public ActionResult Index()
+        {
+            var technicians = db.Technicians.Include(t => t.AspNetUser);
+            return View(technicians.ToList());
+        }
+
+        // GET: Technicians/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Technician technician = db.Technicians.Find(id);
+            if (technician == null)
+            {
+                return HttpNotFound();
+            }
+            return View(technician);
+        }
+
+        // GET: Technicians/Create
+        public ActionResult Create()
+        {
+            ViewBag.user_id = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Technicians/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,user_id,reputation,thumbnail,biography")] Technician technician)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Technicians.Add(technician);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.user_id = new SelectList(db.AspNetUsers, "Id", "Email", technician.user_id);
+            return View(technician);
+        }
+
+        // GET: Technicians/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Technician technician = db.Technicians.Find(id);
+            if (technician == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.user_id = new SelectList(db.AspNetUsers, "Id", "Email", technician.user_id);
+            return View(technician);
+        }
+
+        // POST: Technicians/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,user_id,reputation,thumbnail,biography")] Technician technician)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(technician).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.user_id = new SelectList(db.AspNetUsers, "Id", "Email", technician.user_id);
+            return View(technician);
+        }
+
+        // GET: Technicians/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Technician technician = db.Technicians.Find(id);
+            if (technician == null)
+            {
+                return HttpNotFound();
+            }
+            return View(technician);
+        }
+
+        // POST: Technicians/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Technician technician = db.Technicians.Find(id);
+            db.Technicians.Remove(technician);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
